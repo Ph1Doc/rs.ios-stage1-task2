dd48c3295681374384d87a4143b51398b7ae202c
Oleg Sapun <oleg.sapun@gmail.com>
2020-03-22 14:29:01 +0300

    feat: add new parameters textForDay

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -3,7 +3,22 @@
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+
+    NSString *stringFromString = [[NSString alloc] initWithFormat:@"%@-%@-%@", year, month, day];
+    NSLog(@"%@", stringFromString);
+
+    NSDateFormatter *formatterFromStringToDate = [[NSDateFormatter alloc] init];
+    formatterFromStringToDate.dateFormat = @"yyyy-MM-dd";
+
+    NSDate *dateFromString = [formatterFromStringToDate dateFromString:stringFromString];
+
+    NSDateFormatter *formatterFromDateToString = [[NSDateFormatter alloc] init];
+    formatterFromDateToString.dateFormat = @"dd MMMM, EEEE";
+    formatterFromDateToString.locale = [NSLocale localeWithLocaleIdentifier:@"ru_RU"];
+
+    NSString *dateString = [formatterFromDateToString stringFromDate:dateFromString];
+
+    return dateString != nil ? dateString : @"Такого дня не существует";
 }
 
 @end
