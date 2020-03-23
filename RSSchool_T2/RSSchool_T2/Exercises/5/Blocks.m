793c23b5cac4ca9767398a351570d9c58d912ea0
Oleg Sapun <oleg.sapun@gmail.com>
2020-03-23 23:49:22 +0300

    feat: add new function Blocks

--- RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
+++ RSSchool_T2/RSSchool_T2/Exercises/5/Blocks.m
@@ -1,7 +1,66 @@
 #import "Blocks.h"
 
+@interface Blocks ()
+
+@property (nonatomic, strong) NSArray *array;
+
+@end
+
 @implementation Blocks
 
+- (instancetype)init
+{
+    self = [super init];
+
+    if (self) {
+        self.blockA = ^(NSArray* num) {
+            self.array = num;
+        };
+
+        self.blockB = ^(Class class) {
+            NSMutableArray *objects = [[NSMutableArray alloc] init];
+
+            for (NSObject *obj in self.array) {
+                if ([obj isKindOfClass:class]) {
+                    [objects addObject:obj];
+                }
+            }
+
+            if ([class isEqual:[NSNumber class]]) {
+                self.blockC([Blocks sum:objects]);
+            } else if ([class isEqual:[NSString class]]) {
+                self.blockC([objects componentsJoinedByString:@""]);
+            } else if ([class isEqual:[NSDate class]]) {
+                self.blockC([Blocks date:objects]);
+            } else {
+                self.blockC(objects);
+            }
+        };
+    }
+
+    return self;
+}
+
++ (NSObject*)sum:(NSArray *)array {
+    int sum = 0;
+    for (int i = 0; i < [array count]; i++) {
+        sum = sum + [array[i] intValue];
+    }
+    return [NSNumber numberWithInt:sum];
+}
+
++ (NSObject*)date:(NSArray *)array {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.dateFormat = @"dd.MM.yyyy";
+
+    NSDate *date = [array firstObject];
+
+    for (NSDate *currentDate in array) {
+        date = [currentDate laterDate:date];
+    }
+    
+    return [formatter stringFromDate:date];
+}
 
 @end
 
