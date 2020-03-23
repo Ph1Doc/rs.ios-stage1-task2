714ba34b780ea529517445318f93582c94777388
Oleg Sapun <oleg.sapun@gmail.com>
2020-03-23 12:20:49 +0300

    feat: add new function abbreviationFromA

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,28 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+
+    if (a.length < 1 || b.length < 1 || a.length > 1000 || b.length > 1000) {
+        return @"NO";
+    }
+
+    NSMutableString *upercaseA = [[NSMutableString alloc] initWithString:[a uppercaseString]];
+    NSMutableString *upercaseB = [[NSMutableString alloc] initWithString:[b uppercaseString]];
+
+    NSUInteger elementInB = [b length];
+    for (int i = 0; i < [upercaseB length]; i++) {
+         for (int j = 0; j < [upercaseA length]; j++) {
+             if ([upercaseB substringWithRange:NSMakeRange(i, 1)] == [upercaseA substringWithRange:NSMakeRange(j, 1)]) {
+                 upercaseA = [[upercaseA substringWithRange:NSMakeRange(j, upercaseA.length - j)] mutableCopy];
+                 elementInB = elementInB - 1;
+             }
+         }
+    }
+
+    if (elementInB == 0) {
+        return @"YES";
+    } else {
+        return @"NO";
+    }
 }
 @end
